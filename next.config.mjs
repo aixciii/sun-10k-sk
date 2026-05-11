0041f673c3b9f817d076a718615330e8ff27bc56
aixciii <artema02@gmail.com>
2026-05-11 10:08:23 +0200

    `Remove www redirect, keep canonical tag`

--- next.config.mjs
+++ next.config.mjs
@@ -18,16 +18,6 @@ const nextConfig = {
       },
     ],
   },
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'www.sun-10k.sk' }],
-        destination: 'https://sun-10k.sk/:path*',
-        permanent: true,
-      },
-    ]
-  },
 }
 
 export default nextConfig
